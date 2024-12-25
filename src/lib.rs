0e04eba712bdd282b6db9a0c54727fd405778795
Oliver Heilmann <hominus@git.hub>
2024-12-25 15:18:27 +0100

    Inline distance and norm functions and make them easier to vectorize.

--- src/lib.rs
+++ src/lib.rs
@@ -24,15 +24,20 @@ impl<T, const N: usize> NeighbourhoodParams<'_, T, N> {
     }
 }
 
+#[inline]
 fn distance<T: Float, const N: usize>(v1: &[T; N], v2: &[T; N]) -> T {
-    let dst_squared = v1
-        .iter()
-        .zip(v2.iter())
-        .fold(T::zero(), |acc, (v1, v2)| acc + (*v1 - *v2).powi(2));
-    dst_squared.sqrt()
+    let mut dst = T::zero();
+    for i in 0..N {
+        dst = dst + (v1[i] - v2[i]).powi(2);
+    }
+    dst.sqrt()
 }
 
+#[inline]
 fn norm<T: Float, const N: usize>(v: &[T; N]) -> T {
-    let norm_squared = v.iter().fold(T::zero(), |acc, x| acc + (*x).powi(2));
-    norm_squared.sqrt()
+    let mut norm = T::zero();
+    for i in 0..N {
+        norm = norm + v[i].powi(2);
+    }
+    norm.sqrt()
 }
