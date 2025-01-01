121eca0548e9e4a0a4b9d9b751afb6c9c3aa9fb3
Oliver Heilmann <hominus@git.hub>
2025-01-01 11:52:23 +0100

    Neighbourhood verification test now takes epsilon as an parameter.

--- compare/src/bin/test.rs
+++ compare/src/bin/test.rs
@@ -16,9 +16,12 @@ struct Args {
 
     #[arg(long)]
     kdtree: Vec<String>,
+
+    #[arg(long)]
+    eps: f64
 }
 
-fn run_test<const N: usize>(num_points: usize, iterations: u64, kdtrees: &[String]) {
+fn run_test<const N: usize>(num_points: usize, iterations: u64, kdtrees: &[String], eps: f64) {
     for seed in 0..iterations {
         println!("Seed {seed}");
         let points: Vec<[f64; N]> = make_points(num_points, -10., 10., seed);
@@ -51,7 +54,6 @@ fn run_test<const N: usize>(num_points: usize, iterations: u64, kdtrees: &[Strin
             }
         }
 
-        let eps = 0.1;
         for p in points.iter() {
             let result: Vec<_> = kdtree_list
                 .iter()
@@ -117,22 +119,22 @@ fn main() {
     let args = Args::parse();
 
     match args.dim {
-        1 => run_test::<1>(args.num_points, args.iterations, &args.kdtree),
-        2 => run_test::<2>(args.num_points, args.iterations, &args.kdtree),
-        3 => run_test::<3>(args.num_points, args.iterations, &args.kdtree),
-        4 => run_test::<4>(args.num_points, args.iterations, &args.kdtree),
-        5 => run_test::<5>(args.num_points, args.iterations, &args.kdtree),
-        6 => run_test::<6>(args.num_points, args.iterations, &args.kdtree),
-        7 => run_test::<7>(args.num_points, args.iterations, &args.kdtree),
-        8 => run_test::<8>(args.num_points, args.iterations, &args.kdtree),
-        9 => run_test::<9>(args.num_points, args.iterations, &args.kdtree),
-        10 => run_test::<10>(args.num_points, args.iterations, &args.kdtree),
-        11 => run_test::<11>(args.num_points, args.iterations, &args.kdtree),
-        12 => run_test::<12>(args.num_points, args.iterations, &args.kdtree),
-        13 => run_test::<13>(args.num_points, args.iterations, &args.kdtree),
-        14 => run_test::<14>(args.num_points, args.iterations, &args.kdtree),
-        15 => run_test::<15>(args.num_points, args.iterations, &args.kdtree),
-        16 => run_test::<16>(args.num_points, args.iterations, &args.kdtree),
+        1 => run_test::<1>(args.num_points, args.iterations, &args.kdtree, args.eps),
+        2 => run_test::<2>(args.num_points, args.iterations, &args.kdtree, args.eps),
+        3 => run_test::<3>(args.num_points, args.iterations, &args.kdtree, args.eps),
+        4 => run_test::<4>(args.num_points, args.iterations, &args.kdtree, args.eps),
+        5 => run_test::<5>(args.num_points, args.iterations, &args.kdtree, args.eps),
+        6 => run_test::<6>(args.num_points, args.iterations, &args.kdtree, args.eps),
+        7 => run_test::<7>(args.num_points, args.iterations, &args.kdtree, args.eps),
+        8 => run_test::<8>(args.num_points, args.iterations, &args.kdtree, args.eps),
+        9 => run_test::<9>(args.num_points, args.iterations, &args.kdtree, args.eps),
+        10 => run_test::<10>(args.num_points, args.iterations, &args.kdtree, args.eps),
+        11 => run_test::<11>(args.num_points, args.iterations, &args.kdtree, args.eps),
+        12 => run_test::<12>(args.num_points, args.iterations, &args.kdtree, args.eps),
+        13 => run_test::<13>(args.num_points, args.iterations, &args.kdtree, args.eps),
+        14 => run_test::<14>(args.num_points, args.iterations, &args.kdtree, args.eps),
+        15 => run_test::<15>(args.num_points, args.iterations, &args.kdtree, args.eps),
+        16 => run_test::<16>(args.num_points, args.iterations, &args.kdtree, args.eps),
         _ => panic!(),
     }
 
